293f813a4859bec381201b4e6bbf7b8ad671d67b
samiulalimsaad <saadnbiuraj@gmail.com>
2022-06-08 11:56:18 +0600

    responsive

--- src/Hero.tsx
+++ src/Hero.tsx
@@ -8,13 +8,13 @@ const Hero = () => {
             className="min-h-[120vh] hero justify-start bg-[url('/images/hero.svg')]"
         >
             <div className="w-5/12 text-base-100">
-                <h1 className="text-6xl">
+                <h1 className="text-3xl sm:text-4xl md:text-5xl xl:text-6xl">
                     Your Awesome Traffic permit consultant
                 </h1>
-                <button className="mt-8 btn btn-error text-base-100">
+                <button className="flex mt-8 btn btn-error text-base-100">
                     Get Started
                     <span>
-                        <ArrowNarrowRightIcon className="w-6 h-6 ml-3" />
+                        <ArrowNarrowRightIcon className="w-4 h-4 ml-3 sm:w-6 sm:h-6" />
                     </span>
                 </button>
             </div>
