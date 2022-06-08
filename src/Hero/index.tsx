75e6f75e076bc868f42cce0042b746e1d78d9678
samiulalimsaad <saadnbiuraj@gmail.com>
2022-06-08 14:04:59 +0600

    reorder files

--- src/Hero/index.tsx
+++ src/Hero/index.tsx
@@ -1,12 +1,13 @@
 import { ArrowNarrowRightIcon } from "@heroicons/react/solid";
 import React from "react";
+import Truck from "./Truck";
 
 const Hero = () => {
     return (
         <section
             id="hero"
             data-aos="fade-down"
-            className="min-h-[120vh] hero justify-start bg-[url('/images/hero.svg')]"
+            className="relative min-h-[120vh] hero justify-start bg-[url('/images/hero.svg')]"
         >
             <div className="w-5/12 text-base-100">
                 <h1 className="text-3xl sm:text-4xl md:text-5xl xl:text-6xl">
@@ -18,6 +19,9 @@ const Hero = () => {
                         <ArrowNarrowRightIcon className="w-4 h-4 ml-3 sm:w-6 sm:h-6" />
                     </span>
                 </button>
+            </div>
+            <div className="absolute left-0 -bottom-40">
+                <Truck />
             </div>
         </section>
     );
