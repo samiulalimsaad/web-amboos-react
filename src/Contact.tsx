20691ba7b66eb0231520b6760f8bde9a1458eafb
samiulalimsaad <saadnbiuraj@gmail.com>
2022-06-08 14:05:17 +0600

    fixed

--- src/Contact.tsx
+++ src/Contact.tsx
@@ -1,20 +1,20 @@
 import { ArrowNarrowRightIcon } from "@heroicons/react/solid";
 import React from "react";
-import Truck from "./Navbar/Truck";
+import Truck from "./Hero/Truck";
 
 const Contact = () => {
     return (
         <section
             id="contactus"
-            className="relative mt-40 w-11/12 min-h-[35rem] rounded-tr-[10rem] hero bg-slate-900"
+            className="relative mt-40 w-11/12 min-h-[30rem] rounded-tr-[10rem] hero bg-slate-900"
         >
             <div>
                 <div>
-                    <div className="absolute left-0 -top-52 w-96">
+                    <div className="absolute left-0 -top-48 w-96">
                         <Truck />
                     </div>
                 </div>
-                <div className="absolute w-3/12 p-8 font-semibold leading-7 border-l-8 rounded-lg left-52 top-40 border-error text-base-100">
+                <div className="absolute w-3/12 p-8 font-semibold leading-7 border-l-8 rounded-lg left-52 top-20 border-error text-base-100">
                     <p>
                         We provide traffic management consultants so you get
                         started quickly. COntact us for a quote today!
@@ -53,7 +53,7 @@ const Contact = () => {
             </div>
 
             {/* nav */}
-            <div className="container flex justify-between w-full px-40 mx-auto mt-96">
+            <div className="container flex justify-between w-full px-40 mx-auto mt-72">
                 <a
                     href="#hero"
                     className="text-xl font-black uppercase btn btn-ghost text-base-100"
