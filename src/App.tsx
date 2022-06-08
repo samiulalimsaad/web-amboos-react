056dccd8c3412102589ef713bda24fb7710cca49
samiulalimsaad <saadnbiuraj@gmail.com>
2022-06-08 12:32:21 +0600

    footer added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import About from "./About";
+import Footer from "./Footer";
 import Hero from "./Hero";
 import HowTo from "./HowTo";
 import Navbar from "./Navbar";
@@ -15,6 +16,7 @@ function App() {
             <Truck />
             <About />
             <HowTo />
+            <Footer />
         </>
     );
 }
